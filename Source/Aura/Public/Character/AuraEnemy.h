75b25c146792c946ec4c4703336b5dd5d29fb02c
saz5755 <saz575593@gmail.com>
2024-12-17 03:06:05 +0900

    몬스터의 호버링할것, 하이라이트 함수를 다양하게 하기 위함과 종속되지 않게하기 위해 인터페이스로 추가 인터페이스는 순수가상함수로 정의하여 적용

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -2,11 +2,16 @@
 
 #include "CoreMinimal.h"
 #include "AuraCharacterBase.h"
+#include "Interaction/EnemyInterface.h"
 #include "AuraEnemy.generated.h"
 
 UCLASS()
-class AURA_API AAuraEnemy : public AAuraCharacterBase
+class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 {
 	GENERATED_BODY()
+
+public:
+	virtual void HighlightActor() override;
+	virtual void UnHighlightActor() override;
 	
 };
