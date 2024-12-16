6728cdfe12e20577f8ccd78f6cf46b34f3640ff0
saz5755 <saz575593@gmail.com>
2024-12-17 01:23:17 +0900

    캐릭터, Goblin_Spear의 Weapon을 스켈레탈 메시를 이용하여 Hand Socket에  Attach하여 적용

---
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -0,0 +1,14 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraCharacterBase.h"
+#include "AuraCharacter.generated.h"
+
+
+UCLASS()
+class AURA_API AAuraCharacter : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraCharacterBase.h"
+#include "AuraEnemy.generated.h"
+
+UCLASS()
+class AURA_API AAuraEnemy : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
